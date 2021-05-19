2870ba82ddafc384a50024aa95bfca0b50703d0f
gabrielsc.dev <gabrielsc.dev@gmail.com>
2021-05-19 19:51:07 -0300

    Implementação do construtor.

--- src/Insumos.cpp
+++ src/Insumos.cpp
@@ -2,7 +2,14 @@
 
 Insumos::Insumos()
 {
-    //ctor
+    nome = "NULL";
+    quantidade = 0;
+    valor_unitario = 0;
+    data_vencimento = "00/00/0000";
+    nome_fab = "NULL";
+    tipoInsumo = "INDEFINIDO";
+    local = "INDEFINIDO";
+    codigo_unico = "NULL";
 }
 
 Insumos::~Insumos()
